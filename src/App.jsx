c7b043addf6a7f9d74bc4ff60a9b4f6417f7a33f
Caio Lemec <caiolemec@gmail.com>
2023-11-13 10:40:40 -0300

    creating sidebar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Caio Lemec" content="lorem" />
+        </main>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import styles from "./Sidebar.module.css";
+import { PencilSimpleLine } from "phosphor-react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1562813733-b31f71025d54?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/CaioLemec.png" />
+        <strong>Caio Lemec</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilSimpleLine size={20} />
+          Edit your profile
+        </a>
+      </footer>
+    </aside>
+  );
+}
